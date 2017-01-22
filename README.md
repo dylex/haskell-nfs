af616ec42d29e7fd7594269573a6be7393bbdea7
Dylan Simon <dylan@dylex.net>
2017-01-21 21:00:47 -0500

    Improve README.md

---
+++ README.md
@@ -0,0 +1,45 @@
+# Haskell ONC (Sun) RPC and XDR tools and library
+
+Haskell tools and library for interacting with ONC RPC (aka Sun RPC) protocols.
+The goal is a working NFS4 client, so TCP clients and efficient large
+data handling will be prioritized over servers, UDP, and portmaping.
+It includes the following complete components.
+
+## Primary Components
+
+### hsrpcgen
+
+A code-generator for .x (XDR) files which you can call directly or
+use as a pre-processor from cabal (see Setup.hs for an example).  The module
+Network.ONCRPC.Prot in this package is itself generated from the XDR
+description of the RPC protocol.  While many .x files will work as-is, some
+depend on C-specific functionality that will require some minor tweaking.
+
+### Network.ONCRPC.Client
+
+A client library, Network.ONCRPC.Client, which allows you to use the generated
+module to make RPC calls to servers.
+
+## Example
+
+```
+hsrpcgen -P nfs.x -o NFS.hs
+```
+
+```haskell
+import qualified Network.ONCRPC as RPC
+import qualified NFS
+
+client <- RPC.openClient $ RPC.ClientServerPort "nfs.server.example.com" "nfs"
+reply <- RPC.rpcCall client $ RPC.Call (NFS.nFSPROC_NULL (NFS.nFS_VERSION NFS.nFS_PROGRAM)) RPC.AuthNone RPC.AuthNone ()
+RPC.closeClient client
+```
+
+## References
+
+This package implements [RFC5531](https://tools.ietf.org/html/rfc5531) and
+[RFC4506](https://tools.ietf.org/html/rfc4506) as closely as possible.
+
+[xdrgen](http://github.com/jthornber/xdrgen) is a rpcgen replacement for
+other languages, written in Haskell.  By contrast, this project targets full RPC
+support in Haskell.
--- tools/hsrpcgen.hs
+++ tools/hsrpcgen.hs
@@ -1,23 +1,29 @@
 {-# LANGUAGE RecordWildCards #-}
-import           Data.List (foldl')
+import           Data.List (foldl', intercalate)
 import           Data.Maybe (fromMaybe)
 import qualified System.Console.GetOpt as Opt
 import           System.Environment (getProgName, getArgs)
 import           System.Exit (exitFailure)
+import           System.FilePath (splitExtension, splitDirectories)
 import           System.IO (hPutStrLn, stderr)
 
 import           Network.ONCRPC.XDR.Generate
 
 data Opts = Opts
   { optGenerate :: GenerateOptions
+  , optOutput :: FilePath
   }
+
+defModuleName :: String
+defModuleName = "Prot"
 
 defOpts :: Opts
 defOpts = Opts
   { optGenerate = GenerateOptions
-    { generateModuleName = "Prot"
+    { generateModuleName = defModuleName
     , generateReidentOptions = defaultReidentOptions
     }
+  , optOutput = "-"
   }
 
 setGenerate :: (GenerateOptions -> GenerateOptions) -> Opts -> Opts
@@ -26,11 +32,23 @@ setGenerate f o = o{ optGenerate = f (optGenerate o) }
 setReident :: (ReidentOptions -> ReidentOptions) -> Opts -> Opts
 setReident f = setGenerate $ \g -> g{ generateReidentOptions = f (generateReidentOptions g) }
 
+setOutput :: FilePath -> Opts -> Opts
+setOutput f o@Opts{ optGenerate = g } = o
+  { optOutput = f
+  , optGenerate = if generateModuleName g == defModuleName && e == ".hs"
+                    then g{ generateModuleName = m } else g
+  } where
+  (b, e) = splitExtension f
+  m = intercalate "." $ splitDirectories b
+
 opts :: [Opt.OptDescr (Opts -> Opts)]
 opts =
-  [ Opt.Option "m" ["module"]
+  [ Opt.Option "o" ["output"]
+      (Opt.ReqArg setOutput "FILE")
+      "write the output to FILE [stdout]"
+  , Opt.Option "m" ["module"]
       (Opt.ReqArg (\a -> setGenerate $ \g -> g{ generateModuleName = a }) "NAME")
-      ("set the generated module name [" ++ generateModuleName (optGenerate defOpts) ++ "]")
+      ("set the generated module name [" ++ defModuleName ++ " or from output filename]")
   , Opt.Option "P" ["procedures-unique"]
       (Opt.NoArg (setReident $ \r -> r{ reidentJoinProcedure = Nothing }))
       "assume program version and procedure names are unique: don't add prefixes"
@@ -45,6 +63,10 @@ opts =
       "make identifiers upper-case by prefixing them"
   ]
 
+output :: FilePath -> String -> IO ()
+output "-" = putStrLn
+output f = writeFile f
+
 main :: IO ()
 main = do
   prog <- getProgName
@@ -55,4 +77,4 @@ main = do
       mapM_ (hPutStrLn stderr) err
       hPutStrLn stderr $ Opt.usageInfo ("Usage: " ++ prog ++ " [OPTION...] FILE") opts
       exitFailure
-  putStrLn =<< generateFromFile optGenerate f
+  output optOutput =<< generateFromFile optGenerate f
