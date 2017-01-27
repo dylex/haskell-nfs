cfee98c69e6107521bc47e8d3bece10f05a038db
Dylan Simon <dylan@dylex.net>
2017-01-27 00:45:50 -0500

    Add stack.yaml; expose Exception module

--- Network/NFS/V4.hs
+++ Network/NFS/V4.hs
@@ -2,6 +2,7 @@ module Network.NFS.V4
   ( module Network.NFS.V4.Prot
   , module Network.NFS.V4.Attr
   , module Network.NFS.V4.Client
+  , module Network.NFS.V4.Exception
   , module Network.NFS.V4.File
   , module Network.NFS.V4.Ops
   , module Network.NFS.V4.String
@@ -10,6 +11,7 @@ module Network.NFS.V4
 import Network.NFS.V4.Prot hiding (SECINFO_NO_NAME4res)
 import Network.NFS.V4.Attr
 import Network.NFS.V4.Client
+import Network.NFS.V4.Exception
 import Network.NFS.V4.File
 import Network.NFS.V4.Ops
 import Network.NFS.V4.String
