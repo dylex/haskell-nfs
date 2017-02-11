62c36566b8eb2790a99016395ebeeb02127727e2
Dylan Simon <dylan@dylex.net>
2017-02-11 16:59:08 -0500

    Basic if header handling

--- webdav/Network/WebDAV/NFS/If.hs
+++ webdav/Network/WebDAV/NFS/If.hs
@@ -12,7 +12,7 @@ import qualified Data.ByteString.Char8 as BSC
 import qualified Network.HTTP.Types as HTTP
 import qualified Network.HTTP.Types.Header as HTTP
 import           Network.URI (URI, parseURIReference)
-import           Waimwork.HTTP (parseHTTPDate, ETag, etagParser, parseETags, matchETag)
+import           Waimwork.HTTP (parseHTTPDate, ETag, etagParser, parseETags, matchETag, strongETagEq)
 
 import           Network.WebDAV.NFS.Types
 import           Network.WebDAV.NFS.Request
@@ -30,42 +30,53 @@ parseURI :: AP.Parser URI
 parseURI = maybe (fail "invalid URI") return . parseURIReference . BSC.unpack
   =<< APC.char '<' *> AP.takeWhile (AP.inClass "!#-;=?-~") <* APC.char '>'
 
-parseIfCondition :: AP.Parser IfCondition
-parseIfCondition = do
-  neg <- AP.option False (True <$ AP.string "Not")
-  APC.skipSpace
-  (IfStateToken neg <$> parseURI
-    <|> IfEntityTag neg <$> (APC.char '[' *> etagParser <* APC.char ']'))
-    <* APC.skipSpace
-
-parseIfList :: AP.Parser [IfCondition]
-parseIfList = APC.char '(' *> APC.skipSpace *> AP.many1 parseIfCondition <* APC.char ')' <* APC.skipSpace
-
-parseTaggedList :: Maybe URI -> AP.Parser [IfList]
-parseTaggedList tag = do
-  tag' <- AP.option tag $ Just <$> parseURI
-  APC.skipSpace
-  (:) . IfList tag' <$> parseIfList <*> AP.option [] (parseTaggedList tag')
-
-parseIfHeader :: AP.Parser IfHeader
-parseIfHeader = APC.skipSpace *> parseTaggedList Nothing <* AP.endOfInput
-
 parseIf :: BS.ByteString -> Either String IfHeader
-parseIf = AP.parseOnly parseIfHeader
+parseIf = AP.parseOnly parseIfHeader where
+
+  parseIfHeader :: AP.Parser IfHeader
+  parseIfHeader = APC.skipSpace *> parseTaggedList Nothing <* AP.endOfInput
+
+  parseTaggedList :: Maybe URI -> AP.Parser [IfList]
+  parseTaggedList tag = do
+    tag' <- AP.option tag $ Just <$> parseURI
+    APC.skipSpace
+    (:) . IfList tag' <$> parseIfList <*> AP.option [] (parseTaggedList tag')
+
+  parseIfList :: AP.Parser [IfCondition]
+  parseIfList = APC.char '(' *> APC.skipSpace *> AP.many1 parseIfCondition <* APC.char ')' <* APC.skipSpace
+
+  parseIfCondition :: AP.Parser IfCondition
+  parseIfCondition = do
+    neg <- AP.option False (True <$ AP.string "Not")
+    APC.skipSpace
+    (IfStateToken neg <$> parseURI
+      <|> IfEntityTag neg <$> (APC.char '[' *> etagParser <* APC.char ']'))
+      <* APC.skipSpace
+
+testIf :: Context -> IfHeader -> Bool
+testIf Context{ contextFile = FileInfo{..} } = any testIfList where
+
+  testIfList (IfList (Just _) _) = False -- TODO: other resource
+  testIfList (IfList Nothing l) = all testIfCondition l
+
+  testIfCondition (IfStateToken n _) = n -- TODO: token
+  testIfCondition (IfEntityTag n e) = n /= (e == fileETag)
 
 checkIfHeaders :: Context -> Maybe HTTP.Status
 checkIfHeaders ctx@Context{ contextFile = FileInfo{..} }
   | not (isnomat || ismod) = Just HTTP.notModified304
   | not (ismat || isnomod) = Just HTTP.preconditionFailed412
+  | not ishead             = Just HTTP.preconditionFailed412
   | otherwise = Nothing
   where
-  ismat   = all (matchETag fileETag) ifmat
-  isnomat = all (not . matchETag fileETag) ifnomat
+  ismat   = all (matchETag (strongETagEq fileETag)) ifmat
+  isnomat = all (not . matchETag (strongETagEq fileETag)) ifnomat
   ismod   = all (fileMTime >) ifmod
   isnomod = all (fileMTime <=) ifnomod
+  ishead  = all (testIf ctx) ifhead
   ifmat   = parseETags    <$> header HTTP.hIfMatch
   ifnomat = parseETags    <$> header HTTP.hIfNoneMatch
   ifmod   = parseHTTPDate =<< header HTTP.hIfModifiedSince
   ifnomod = parseHTTPDate =<< header HTTP.hIfUnmodifiedSince
-  _ifhead  = either (const Nothing) Just . parseIf =<< header "If"
+  ifhead  = either (const Nothing) Just . parseIf =<< header "If"
   header = requestHeader ctx
